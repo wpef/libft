806c7995096d1ffa5212cfc4c6c1d5c1cd9849d3
Foulques DE MONCADE <fde-monc@e3r6p13.42.fr>
2015-12-05 00:22:04 +0100

    retour racine + trim + makefile

--- libft/ft_strsub.c
+++ libft/ft_strsub.c
@@ -1,28 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fde-monc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/12/04 19:54:58 by fde-monc          #+#    #+#             */
-/*   Updated: 2015/12/04 20:40:51 by fde-monc         ###   ########.fr       */
+/*   Created: 2015/12/04 18:18:37 by fde-monc          #+#    #+#             */
+/*   Updated: 2015/12/05 00:15:33 by fde-monc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static	int	isblank (const char s, size_t i)
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
 {
-	if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
-		return (0);
-	else
-		return (1);
-}
+	size_t		i;
+	char	*s1;
 
-char	*ft_strtrim (char const *s)
-{
-	while (isblank(s, i) == 0)
+	i = 0;
+	if (!s)
+		return (NULL);
+	s1 = ft_strnew(len);
+	while (i < len)
+	{
+		s1[i] = s[start];
+		start++;
 		i++;
-
+	}
+	return (s1);
 }
+
