4b1f0268833f85f73f9fa437d0bbf228b1ab3901
Foulques DE MONCADE <fde-monc@e3r6p13.42.fr>
2015-12-05 01:15:47 +0100

    trim OK + make test

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,15 +6,15 @@
 /*   By: fde-monc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/04 19:54:58 by fde-monc          #+#    #+#             */
-/*   Updated: 2015/12/05 00:19:38 by fde-monc         ###   ########.fr       */
+/*   Updated: 2015/12/05 01:14:21 by fde-monc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static	int	isblank (const char s, size_t i)
+static	int	ft_isskip (char s)
 {
-	if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
+	if (s == ' ' || s == '\t' || s == '\n')
 		return (1);
 	else
 		return (0);
@@ -25,13 +25,20 @@ char	*ft_strtrim (char const *s)
 	size_t start;
 	size_t len;
 	char *res;
-	
+
+	if (!s)
+		return(NULL);	
 	start = 0;
-	len = 0;
-	while (isblank(s, start) == 1)
+	len = ft_strlen(s);
+	while (ft_isskip(s[start]) == 1)
 		start++;
-	while(isblank s[start + len] == 0)
-		len++;
-	res = ft_strsub(s, start, len);
+	len--;
+	while(len > 0 && ft_isskip(s[len]) == 1)
+		len--;
+	if (len >= start)
+		res = ft_strsub(s, start, len - start + 1);
+	else
+		return (ft_strdup(""));
+	return (res);
 }
 
