89ab94db7565f2477d33d1a6389c88a9d13c2330
Foulques DE MONCADE <fde-monc@e3r8p10.42.fr>
2015-12-21 19:01:18 +0100

    + ft_isvalid, need ft_touch

--- Fillit/ft_readfile.c
+++ Fillit/ft_readfile.c
@@ -6,20 +6,17 @@
 /*   By: fde-monc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/16 15:39:15 by fde-monc          #+#    #+#             */
-/*   Updated: 2015/12/18 17:37:51 by fde-monc         ###   ########.fr       */
+/*   Updated: 2015/12/21 18:59:06 by fde-monc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "fillit.h"
 #include "libft.h"
 #include <fcntl.h>
 #include <stdio.h>
 
 // if blocks = 4
 // + les pieces doivent se toucher 6 ou 8 fois (en tout)
-
-typedef struct { 
-	char	tetrim[4][5];
-}			t_tris; 
 
 t_tris	*ft_maketab(char *buf)
 {
@@ -31,8 +28,7 @@ t_tris	*ft_maketab(char *buf)
 	col = 0;
 	line = 0;
 	i = 0;
-	tetrimino = (t_trim *)malloc(sizeof(t_trim));
-	printf("BUF = %s\n", buf);
+	tetrimino = (t_tris *)malloc(sizeof(t_tris));
 	while (i < 20)
 	{
 		printf("_____________ DO WHILE 1 _________\n");
@@ -56,20 +52,60 @@ t_tris	*ft_maketab(char *buf)
 		}
 		printf("i = %d _______ REDO WHILE ? ________\n", i);
 	}
-	printf(" --> NOP, END OF CODE !!");
+	printf(" --> NOP, END OF CODE !!\n");
 	return (tetrimino);
 }
 
-//int	ft_isvalid(char *buf)
-//{
-//}
+int	ft_touch(char *buf)
+{
+	int i;
+
+	while (buf[i] == '.' || buf[i] == '\n')
+		i++;
+	if (buf[i] == '#')
+		// check voisins;
+}
+
+int	ft_isvalid(char *buf)
+{
+	int	i;
+	int	block;
+	int	j;
+	int	touch;
+
+	i = 0;
+	block = 0;
+	while (buf[i])
+	{
+		j = 0;
+		while (buf[i] != '\n')
+		{
+			if (buf[i] == '.' || buf[i] == '#')
+			{
+				if (buf[i] == '#')
+					block++;
+				i++;
+				j++;
+			}
+			else 
+				return (0);
+		}
+		if (buf[i] == '\n' && j == 4)
+			i++;
+		else
+			return (0);
+	}
+	touch = ft_touch(buf);
+	if (block == 4 && (touch == 6 || touch == 8))
+		return (1);
+	return (0);
+}
 
 int	main(int ac, char **av)
 {	
 	int		fd;
 	int		ret;
 	char	buf[21];
-	t_tris
 
 	if (ac < 2)
 	{
@@ -79,7 +115,15 @@ int	main(int ac, char **av)
 	fd = open(av[1], O_RDONLY);
 	ret = read(fd, buf, 21);
 	buf[20] = '\0';
-	if (ret > 20)
-		ft_maketab(buf);
+	if (ret >= 20)
+	{
+		if (ft_isvalid(buf))
+		{
+			ft_putendl("===============> CHECK : buf is considered valid <================");
+			ft_maketab(buf);
+		}
+		else
+			ft_putendl("=/=/=/=/=/=/=/=/=/=/=> BUS IS NOT VALID <=/=/=/=/=/=/=/=/=/=/=/");
+	}
 	return (0);
 }
